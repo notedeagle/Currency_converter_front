fcb041b6497872f324f1d3e1c0d90b352af5f8d2
Dawid <notedeagle@gmail.com>
2021-03-17 18:47:18 +0100

    More configurations

--- server.js
+++ server.js
@@ -4,13 +4,6 @@ const path = require('path');
 
 const app = express();
 
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
 const express = require('express');
 const app = express();
 const app = express();
