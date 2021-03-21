462228e6c2e31d6582432efc911d395366f8f538
Dawid <notedeagle@gmail.com>
2021-03-21 21:43:55 +0100

    Fix api url

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,17 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Config } from 'protractor';
-
-const AUTH_API = 'http://localhost:8080/';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  private apiServerUrl = environment.apiBaseUrl;
+
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<HttpResponse<Config>> {
-    return this.http.post<Config>(AUTH_API + 'login', {
+    return this.http.post<Config>(`${this.apiServerUrl}/login`, {
       username,
       password
     }, {observe: 'response'});
